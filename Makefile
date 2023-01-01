40693b08144222fe7d7e13d82b9fa234157fbab2
Khatthaphone <8247636+khatthaphone@users.noreply.github.com>
2023-01-01 14:59:02 +0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+dev:
+	PORT=2565 DATABASE_URL=postgresql://postgres:postgres@localhost re go run server.go
--- server.go
+++ server.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	// dbConnStr := os.Getenv("DATABASE_URL")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", port)))
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
 }
