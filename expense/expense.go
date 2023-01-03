f5e49d575eaa6d4ab7a569f312442de8efba5c5b
Khatthaphone <8247636+khatthaphone@users.noreply.github.com>
2023-01-03 09:56:16 +0700

    TEST: refactor error message for testing

--- expense/expense.go
+++ expense/expense.go
@@ -3,7 +3,6 @@ package expense
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 
@@ -13,7 +12,8 @@ import (
 )
 
 type Err struct {
-	Message string
+	Message string `json:"message"`
+	Error   string `json:"error"`
 }
 
 type Expense struct {
@@ -39,7 +39,7 @@ func (h *handler) AddExpenseHandler(c echo.Context) error {
 	var e Expense
 	err := c.Bind(&e)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, Err{Message: "Error occurred when binding json from request body", Error: err.Error()})
 	}
 
 	sql := `INSERT INTO expenses(title, amount, note, tags) VALUES($1, $2, $3, $4) RETURNING id`
@@ -47,8 +47,7 @@ func (h *handler) AddExpenseHandler(c echo.Context) error {
 
 	err = row.Scan(&e.ID)
 	if err != nil {
-		log.Fatal("Error adding new expense", err.Error())
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Error occured when binding sql result", Error: err.Error()})
 	}
 
 	return c.JSON(http.StatusCreated, e)
@@ -56,10 +55,9 @@ func (h *handler) AddExpenseHandler(c echo.Context) error {
 }
 
 func (h *handler) GetExpenseHandler(c echo.Context) error {
-	fmt.Printf("Raw param: %v\n", c.Param("id"))
 	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
-		log.Fatal("Unable to find expense with sepcified id", err.Error())
+		return c.JSON(http.StatusBadRequest, Err{Message: "No epense id provided", Error: err.Error()})
 	}
 
 	fmt.Printf("Searching for expense ID: %v\n", id)
@@ -72,8 +70,7 @@ func (h *handler) GetExpenseHandler(c echo.Context) error {
 	var e Expense
 	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, (pq.Array)(&e.Tags))
 	if err != nil {
-		log.Fatal("Unable to find expense with sepcified id", err.Error())
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Error occured when binding sql result", Error: err.Error()})
 	}
 
 	return c.JSON(http.StatusOK, e)
@@ -94,8 +91,7 @@ func (h *handler) UpdateExpenseHandler(c echo.Context) error {
 	var result Expense
 	err = row.Scan(&result.ID, &result.Title, &result.Amount, &result.Note, (pq.Array)(&result.Tags))
 	if err != nil {
-		log.Fatal("Error updating expense", err.Error())
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Error occured when binding sql result", Error: err.Error()})
 	}
 
 	return c.JSON(http.StatusOK, result)
@@ -108,8 +104,7 @@ func (h *handler) GetAllExpensesHandler(c echo.Context) error {
 
 	rows, err := h.db.Query(sql)
 	if err != nil {
-		log.Fatal("Unable to find expenses", err.Error())
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Error occured when querying expenses", Error: err.Error()})
 	}
 
 	var expenses []Expense
@@ -118,8 +113,8 @@ func (h *handler) GetAllExpensesHandler(c echo.Context) error {
 		var e Expense
 		err := rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, (pq.Array)(&e.Tags))
 		if err != nil {
-			log.Fatal("Unable to find expenses", err.Error())
-			return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+
+			return c.JSON(http.StatusInternalServerError, Err{Message: "Error occured when binding sql result", Error: err.Error()})
 		}
 
 		expenses = append(expenses, e)
